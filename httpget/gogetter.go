cbe687074ef62d2de69c77f9da8f6127bea08e23
Aseem Jain <Aseem.Jain@rackspace.com>
2017-08-13 16:29:03 -0500

    concept to do http get request

--- httpget/gogetter.go
+++ httpget/gogetter.go
@@ -1,19 +1,19 @@
 package main
 
-import  (
+import (
 	"fmt"
-	"net/http"
 	"io/ioutil"
 	"log"
+	"net/http"
 )
-
 
 func main() {
 	fmt.Println("go get google")
+	fmt.Print("hello world ")
 	res, err := http.Get("http://www3.google.com")
 	page, _ := ioutil.ReadAll(res.Body)
 	res.Body.Close()
-	fmt.Printf("%s",page)
+	fmt.Printf("%s", page)
 	if err != nil {
 		log.Fatal(err)
 	}
