9028d339586aa936ad51f633f0d768c8900a1587
Aseem Jain <Aseem.Jain@rackspace.com>
2017-08-14 23:43:52 -0500

    TO-DO-app updated to gorilla mux and added basic routes and handle funcs

--- miniProjects/TO-DO-app/server.go
+++ miniProjects/TO-DO-app/server.go
@@ -1,20 +1,33 @@
 package main
 
-
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/todos", TodoIndex)
+	router.HandleFunc("/todos/{todoId}", TodoShow)
 
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
-
-	log.Fatal(http.ListenAndServe(":8888",nil))
+	log.Fatal(http.ListenAndServe(":8888", router))
 }
 
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome!")
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Todo Index!")
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["todoId"]
+	fmt.Fprintln(w, "Todo show:", todoId)
+}
