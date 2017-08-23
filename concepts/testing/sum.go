569265fcc2b1f7665c67c4ca2fd8d7e312b4846b
Aseem Jain <Aseem.Jain@rackspace.com>
2017-08-23 10:58:52 -0500

    TDD added Test Table

--- concepts/testing/sum.go
+++ concepts/testing/sum.go
@@ -2,13 +2,11 @@ package main
 
 import "fmt"
 
-func Sum(a int, b int) (total int){
+func Sum(a int, b int) (total int) {
 	return a + b
 }
 
-
-func main(){
-	x := Sum(2,3)
-	fmt.Print("is the toal sum ", x,)
+func main() {
+	x := Sum(2, 3)
+	fmt.Print("is the toal sum ", x)
 }
-
--- concepts/testing/sum_test.go
+++ concepts/testing/sum_test.go
@@ -1,12 +1,40 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+)
 
-func TestSum(t *testing.T){
-	total := Sum(6,4)
-	if total != 10{
-		t.Errorf("sum was incorrect, got: %d, want : %d ", total,10)
+func TestSumConventional(t *testing.T) {
+	total := Sum(6, 4)
+	if total != 10 {
+		t.Errorf("sum was incorrect, got: %d, want : %d ", total, 10)
 	}
 
 }
 
+func TestSumUsingTestTable(t *testing.T) {
+
+	testtable := []struct {
+		scenario string
+		value1    int
+		value2 	  int
+		expectedValue int
+	}{
+		{"Add positive numbers ",2, 3, 5},
+		{"Add negagive numbers ",-4,-8, -12},
+		{"Add positive number - demo fail  ",4,8, 10},
+		{"Add negative and positive numbers ",10, -3, 7},
+	}
+
+	for _, testRow := range testtable {
+		processedValue := Sum(testRow.value1, testRow.value2)
+		if processedValue != testRow.expectedValue {
+			t.Errorf("  Scenario Failed :: %s  expected %d got %d",testRow.scenario,testRow.expectedValue, processedValue)
+		}else {
+			fmt.Printf(" \n %s :: Scenario Passed, expected %d got %d",testRow.scenario,testRow.expectedValue, processedValue)
+		}
+
+	}
+
+}
