d54c42a66266ce4f1f03ac2414813aaa914dc9ca
Aseem Jain <Aseem.Jain@rackspace.com>
2017-09-13 14:28:50 -0500

    TDD - inserted main code to pass tests

--- concepts/testingTable/calculator.go
+++ concepts/testingTable/calculator.go
@@ -1,1 +1,6 @@
 package main
+
+
+func multiplication(v1 , v2 int) int {
+	return v1 * v2
+}
--- concepts/testingTable/calculator_test.go
+++ concepts/testingTable/calculator_test.go
@@ -11,19 +11,20 @@ func Test_multiplication(t *testing.T) {
 	}
 
 	tests := []struct {
-		inputParams
+		inputParams inputParams
 		expected int
 	}{
 		{inputParams{0, 0}, 0},
 		{inputParams{1, 1}, 1},
+		{inputParams{-2, -2}, 4},
 	}
 
 	for _,useCase := range tests{
-
-		got := multiplication(useCase.v1, useCase.v2)
+		got := multiplication(useCase.inputParams.v1, useCase.inputParams.v2)
 		if got != useCase.expected {
 			t.Errorf("got %v, expected %v \n", got, useCase.expected)
 		}
 	}
+
 
 }
