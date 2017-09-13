90ef85b7b139852cf0dd4bd0ee47fdc5b659f0d8
Aseem Jain <Aseem.Jain@rackspace.com>
2017-09-13 15:50:31 -0500

    chan : basic declaration, initizliation and read write

--- concepts/channel/basic_chanel.go
+++ concepts/channel/basic_chanel.go
@@ -5,26 +5,28 @@ import "fmt"
 func main() {
 
 	var c chan int // declaration
-	fmt.Printf("c = %v", c)
+	fmt.Printf("c = %v\n", c)
 
 	// check length, it is unbuffered so len will be 0
-	fmt.Printf("chan len = %v", len(c))
+	fmt.Printf("chan len = %v\n", len(c))
 
 
 	c = make(chan int, 2)
 	// check length, it is unbuffered so len will be 0
-	fmt.Printf("chan len = %v", len(c))
+	fmt.Printf("chan len = %v\n", len(c))
 
 	// writing to channel
 	c <- 1
 	c <-786
 	//c <- 6162
 
-	fmt.Printf("chan len = %v", len(c))
+	fmt.Printf("chan len = %v\n", len(c))
 
 	var v = <- c
-	fmt.Println("len of c: ", len(c))
+	fmt.Println("len of c: \n", len(c))
 	fmt.Printf("v = %v\n", v)
+
+
 
 }
 
