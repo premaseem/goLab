219ad00df56eae01712290add64ed1c7a2b3fa3b
Aseem Jain <Aseem.Jain@rackspace.com>
2017-09-13 14:40:06 -0500

    TDD - moved test data from method level to global or package level

--- concepts/testingTable/calculator_test.go
+++ concepts/testingTable/calculator_test.go
@@ -1,20 +1,21 @@
 package main
 import "testing"
 
-func Test_multiplication(t *testing.T) {
-	type inputParams struct{
-		v1 int
-		v2 int
-	}
+type inputParams struct{
+	v1 int
+	v2 int
+}
 
-	tests := []struct {
-		inputParams inputParams
-		expected int
-	}{
-		{inputParams{0, 0}, 0},
-		{inputParams{1, 1}, 1},
-		{inputParams{-2, -2}, 4},
-	}
+var tests = []struct {
+	inputParams inputParams
+	expected int
+}{
+	{inputParams{0, 0}, 0},
+	{inputParams{1, 1}, 1},
+	{inputParams{-2, -2}, 4},
+}
+
+func Test_multiplication(t *testing.T) {
 
 	for _,useCase := range tests{
 		t.Run("multiplication tests ", func(t *testing.T) {
@@ -23,6 +24,4 @@ func Test_multiplication(t *testing.T) {
 			}
 		})
 	}
-
-
 }
