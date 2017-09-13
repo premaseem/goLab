2e160a7c6d90b4cfd915744db461e3a6ca0fe115
Aseem Jain <Aseem.Jain@rackspace.com>
2017-09-13 16:37:27 -0500

    producer consumer - chan returning func

--- miniProjects/producerConsumerChannels/main.go
+++ miniProjects/producerConsumerChannels/main.go
@@ -3,26 +3,45 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Using send only channels ")
-	c := make(chan int, 10)
-	c <- 420
-	producer(c)
-	consumer(c)
+	fmt.Println("Functions Returning Channels")
 
+	c := producer()
+	c = consumer1(c)
+	c = consumer2(c)
 
-
-}
-
-func producer(c chan <- int){
-	c <- 1
-	c <- 2
-	c <- 3
-	close(c)
-}
-
-func consumer(c <-chan int ){
-	// reading from channel
 	for v := range c {
-		fmt.Printf("v = %v \n", v)
+		fmt.Printf("\tv = %v\n", v)
 	}
+}
+
+func consumer2(in <-chan int) <-chan int {
+	out := make(chan int, 10)
+	for v := range in {
+		out <- (v * -5) + 100
+	}
+
+	close(out)
+	return out
+}
+
+func consumer1(in <-chan int) <-chan int {
+	out := make(chan int, 10)
+	for v := range in {
+		out <- (v * 2) - 7
+	}
+
+	close(out)
+	return out
+}
+
+func producer() <-chan int {
+	out := make(chan int, 10)
+	out <- 0
+	out <- 1
+	out <- 2
+	out <- 4
+	out <- 8
+	out <- 16
+	close(out)
+	return out
 }
