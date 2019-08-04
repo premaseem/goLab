1d83cd1eabc04f940e95ff12b4b32eaa4b57408a
Aseem Jain <premaseem.webmaster@gmail.com>
2019-08-03 21:20:46 -0500

    custom implementation of interface

--- concepts/httpget/gogetter.go
+++ concepts/httpget/gogetter.go
@@ -2,19 +2,40 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
 	fmt.Println("go get google")
-	fmt.Print("hello world ")
-	res, err := http.Get("http://www3.google.com")
-	page, _ := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	fmt.Printf("%s", page)
+	res, err := http.Get("http://www.google.com")
+
+	// 1. Read using utils
+	//page, _ := ioutil.ReadAll(res.Body)
+	//res.Body.Close()
+	//fmt.Printf("%s", page)
+
+	// 2. Read using Reader
+	//data := make([]byte,99999)
+	//res.Body.Read(data)
+	//fmt.Println(string(data))
+
+	// 3. write to standout
+	//io.Copy(os.Stdout, res.Body)
+
+	// 4. custom write implementation
+	myWriter := custmoWriter{}
+	io.Copy(myWriter, res.Body)
+
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+type custmoWriter struct{}
+
+func (custmoWriter) Write(data []byte) (int, error) {
+	fmt.Println(string(data))
+	return len(data), nil
 }
