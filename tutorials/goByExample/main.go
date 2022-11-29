e674123071821ce2f8a8e697886c2ddbf8adfb56
aseem.jain <aseem.jain@2k.com>
2022-11-28 20:42:43 -0600

    Go Lang Tutorial - Gobyexample

---
+++ tutorials/goByExample/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//var x = "initial"
+	x := "initial value"
+	fmt.Println(x)
+
+	//var a,b,c,d,f = 4,8,12,45
+	//fmt.Println(a,b,c,d,f)
+
+	var e int
+	e = 34
+	fmt.Println(e)
+
+	f := 34
+	fmt.Println(f)
+
+}
---
+++ tutorials/goByExample/print-statements.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+	fmt.Println("I " + "love" + " " + "my mom")
+	fmt.Println(5*2, "multiplication test")
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
